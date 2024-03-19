2dadca27591643019d89563a6954b15f0a78434a
1prcntethan <148378961+1prcntethan@users.noreply.github.com>
2024-03-19 02:33:56 +0000

    commit 5 added mobile page for homepage and did work on study page

---
+++ homepage.js
@@ -0,0 +1,16 @@
+jQuery.noConflict()
+jQuery(function($) {
+    $(document).ready(function() {
+    
+    $('.overlay').click(function(){
+    $('.nav-list').removeClass('show');
+    $('.overlay').removeClass('show');
+    });
+
+    $('.menu-button').click(function(){
+        $('.nav-list').toggleClass('show');
+        $('.overlay').toggleClass('show');
+    });
+    
+});
+});
