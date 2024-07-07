c17f36f5fc5f710d60ad95aa4900e66d2d148c14
1prcntethan <148378961+1prcntethan@users.noreply.github.com>
2024-07-06 22:28:07 -0700

    working on timer

--- general.js
+++ general.js
@@ -15,4 +15,43 @@ jQuery(function($) {
 });
 });
 
+var seconds = document.getElementById("countdown").textContent;
+var countdown;
 
+function addFive()
+{
+    seconds+=5;
+    document.getElementById("countdown").innerHTML = seconds;
+
+}
+
+function minusTime()
+{
+    seconds-=5;
+    document.getElementById("countdown").innerHTML = seconds;
+}
+
+function startTimer()
+{
+    countdown = setInterval(function() {
+        seconds--;
+        if(seconds == 1)
+        {
+            document.getElementById("plural").textContent = "";
+        }else{
+            document.getElementById("plural").textContent = "s";
+        }
+        document.getElementById("countdown").textContent = seconds;
+        if(seconds <= 0)
+        {
+            endTimer();
+        } 
+    }, 1000);
+}
+
+function endTimer()
+{
+    clearInterval(countdown);
+    seconds = 60;
+    document.getElementById("countdown").innerHTML = "60";
+}
